c8104321294f5501af1345884c18979277858cda
Ardyn Sulaeman <lemanfoetra@gmail.com>
2024-04-16 14:57:32 +0700

    methode to redirect to another link inside javascript (Programmatic Navigation)

--- src/components/users/UsersList.vue
+++ src/components/users/UsersList.vue
@@ -1,4 +1,5 @@
 <template>
+  <button @click="toTeamPage()" type="button">To Teams Page</button>
   <ul>
     <user-item v-for="user in users" :key="user.id" :name="user.fullName" :role="user.role"></user-item>
   </ul>
@@ -12,6 +13,11 @@ export default {
     UserItem,
   },
   inject: ['users'],
+  methods: {
+    toTeamPage() {
+      this.$router.push('/teams');
+    }
+  },
 };
 </script>
 
