d04a4acd1bd5599d53492400cebc46f721200326
Ardyn Sulaeman <lemanfoetra@gmail.com>
2024-04-17 10:36:09 +0700

    Redirect in route & catch all not found url

--- src/main.js
+++ src/main.js
@@ -4,13 +4,16 @@ import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
+import NotFound from './components/nav/NotFount.vue'
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
+        { path: '/', redirect: '/teams' },
         { path: '/teams', component: TeamsList },
         { path: '/teams/:teamId', component: TeamMembers, props: true },
         { path: '/users', component: UsersList },
+        { path: '/:notFount(.*)', component: NotFound },
     ],
 })
 
